19e32f2500f8b448fb8cbbacf02a0295325eff2b
Facundo Migliorini <facundo.mig@gmail.com>
2017-09-19 09:35:13 -0300

    Calculo por get usando @PathVariable, genero dos nuevas vistas así no se pierde lo hecho por post y no quedan links erroneos

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCuenta.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCuenta.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +14,30 @@ import ar.edu.unlam.tallerweb1.modelo.Calculadora;
 
 @Controller
 public class ControladorCuenta {
+	
+	@RequestMapping(path="/operation/suma/{num1}/{num2}", method = RequestMethod.GET)
+	public ModelAndView sumar(@PathVariable("num1") int num1, @PathVariable("num2") int num2 ){
+		try{
+			ModelMap modelo = new ModelMap();
+			
+			Calculadora calc = new Calculadora();
+			calc.setNum1(num1);
+			calc.setNum2(num2);
+			Integer suma = calc.sum();
+			
+			modelo.put("num1", calc.getNum1());
+			modelo.put("num2", calc.getNum2());
+			modelo.put("res", suma);			
+			
+			return new ModelAndView("/resultGet", modelo);
+			
+		} catch(Throwable t) {
+			
+			return new ModelAndView("/errorGet");
+		}
+		
+	}
+
 	
 	@RequestMapping(path="/calc")
 	public ModelAndView irAInicio(){
@@ -55,5 +80,6 @@ public class ControladorCuenta {
 	public ModelAndView error(){		
 		return new ModelAndView("error");
 	}
+
 	
 }
